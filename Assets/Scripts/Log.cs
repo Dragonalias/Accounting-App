94d06c6d7b45cdcab50101bdbb2085aac5a9d1fa
Damian Ravn <dragon1600@gmail.com>
2020-05-21 18:51:36 +0200

    Log added to scene

---
+++ Assets/Scripts/Log.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Log : MonoBehaviour
+{
+    [SerializeField] private Color textColor;
+    private void OnEnable()
+    {
+        Application.logMessageReceived += HandleLog;
+    }
+
+    private void OnDisable()
+    {
+        Application.logMessageReceived -= HandleLog;
+    }
+
+    private void HandleLog(string logString, string stackTrace, LogType type)
+    {
+        GameObject newLog = new GameObject("myTextGO");
+        newLog.transform.SetParent(transform);
+        var myText = CreateTextComponent(newLog);
+        
+
+        if (type == LogType.Exception)
+        {
+            myText.text = stackTrace;
+        }
+        else
+        {
+            myText.text = logString;
+        }
+        newLog.GetComponent<RectTransform>().sizeDelta = new Vector2(0, myText.preferredHeight);
+    }
+
+    private TMPro.TextMeshProUGUI CreateTextComponent(GameObject canvasGameObject)
+    {
+        var myText = canvasGameObject.AddComponent<TMPro.TextMeshProUGUI>();
+        myText.color = textColor;
+        myText.enableAutoSizing = true;
+        myText.fontSizeMax = 30;
+
+        return myText;
+    }
+}
